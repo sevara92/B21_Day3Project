ffcf6cebfcf6fe2c0e65723044eb4a7c9245e3fa
Sevara Isroilova <sevara.i.d@gmail.com>
2020-11-12 11:32:58 -0500

    Hour2 3rd commit

--- src/NewClass_Hour2.java
+++ src/NewClass_Hour2.java
@@ -3,7 +3,7 @@ public class NewClass_Hour2 {
     public static void main(String[] args) {
         System.out.println("1 commit at Hour2 class");
 // hour2 2 commit
-
+// hour2 3 commit
 
     }
 }
