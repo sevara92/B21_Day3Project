1fbe7e0239c39d57e8964745fea193d500e0e3fb
Sevara Isroilova <sevara.i.d@gmail.com>
2020-11-12 10:45:29 -0500

    My third committ after creating a project

--- src/Nov12Thursday.java
+++ src/Nov12Thursday.java
@@ -2,6 +2,6 @@ public class Nov12Thursday {
     public static void main(String[] args) {
         System.out.println("Today is the last day of learning Github");
         System.out.println("I have pushed my committ to github now I am addding more changes to my project");
-
+//Third commit
     }
 }
